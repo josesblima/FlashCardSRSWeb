a44ba411337fc7c1c00d31689357ea5405ac0fca
Jose Lima <josel@bluerose-consulting.com>
2026-04-10 16:11:34 +0100

    fixes for tests in pipeline

--- flashcardsrsweb/db/engine.py
+++ flashcardsrsweb/db/engine.py
@@ -1,5 +1,6 @@
 # flashcardsrsweb/db/engine.py
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
+from sqlalchemy.pool import NullPool
 import os
 
 class EngineManager:
@@ -10,11 +11,14 @@ class EngineManager:
         if cls._engine is None:
             database_url = os.getenv("DATABASE_URL")
             async_db_url = database_url.replace("postgresql://", "postgresql+asyncpg://")
-            cls._engine = create_async_engine(
-                async_db_url,
-                pool_pre_ping=True,
-                pool_size=5,
-                pool_timeout=30,
-                max_overflow=10
-            )
+            if os.getenv("TESTING"):
+                cls._engine = create_async_engine(async_db_url, poolclass=NullPool)
+            else:
+                cls._engine = create_async_engine(
+                    async_db_url,
+                    pool_pre_ping=True,
+                    pool_size=5,
+                    pool_timeout=30,
+                    max_overflow=10
+                )
         return cls._engine
